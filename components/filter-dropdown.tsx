4d778bd79edd820afbb682cb86e1b3113064d173
Rafael <fagundesrafael88@gmail.com>
2024-08-23 23:31:52 -0400

    first commit

--- components/filter-dropdown.tsx
+++ components/filter-dropdown.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,18 +7,38 @@ import {
   DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+} from "@/components/ui/dropdown-menu";
 
-import { Button } from '@/components/ui/button';
-import { Filter } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import { Filter } from "lucide-react";
+import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function FilterDropdown() {
+  // state can be "", "pending" or "completed"
+  const [filterStatus, setFilterStatus] = useState<string>("");
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  function handleChangeFilter(e: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (e) {
+      params.set("status", e);
+    } else {
+      params.delete("status");
+    }
+    replace(`${pathName}?${params.toString()}`);
+    setFilterStatus(e);
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant="outline"
-          size={'default'}
+          size={"default"}
           className="flex gap-2 text-slate-600"
         >
           <Filter className="h-4 w-4" />
@@ -29,7 +49,10 @@ export default function FilterDropdown() {
       <DropdownMenuContent className="w-16">
         <DropdownMenuLabel>Filtrar por:</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup value="">
+        <DropdownMenuRadioGroup
+          value={filterStatus}
+          onValueChange={handleChangeFilter}
+        >
           <DropdownMenuRadioItem value="">Todos</DropdownMenuRadioItem>
           <DropdownMenuRadioItem value="pending">
             Pendente
